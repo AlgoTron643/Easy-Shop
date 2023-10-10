28a1c7165c193625ecbcf3b8f5a76c9aa476bbb3
Syed Ahammad Newaz Saif <snewaz@student.unimelb.edu.au>
2023-10-10 18:08:45 +1100

    added the client application with web assembly

--- EasyShop Web Server/Program.cs
+++ EasyShop Web Server/Program.cs
@@ -2,14 +2,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using EasyShop_Web_Server.Data;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); // Can pass in args from command line
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-var app = builder.Build();
+var app = builder.Build(); // We will build the builder
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,15 +18,21 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage(); // We will use the developer exception pages
+}
 
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
-app.UseRouting();
+app.UseRouting(); // Default routing with .NET CORE or .NET routing
 
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+// Add the endpoint for the razor
+// We are adding or configuring the signalR connection (talks to the main application that is running informing changes specially on the UI making it responsive like JS) for our Blazor application
+app.MapBlazorHub(); // Add BlazorHub to the default path, configures everything for you
+app.MapFallbackToPage("/_Host"); // If it does not find anything else to route, then look for a page in "_Host.cshtml" from Pages and see routing in action
 
 app.Run();
 
---
+++ EasyShopWeb_Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using EasyShopWeb_Client;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app"); // Add the root component like VueJs where we have to load the component to the builder => #app in a css file in wwwroot folder
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Only 1 client service using HTTP will retrieve data
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
+
